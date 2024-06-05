e9a4c530561285fa76e96ae591653fd10cff68e7
Moya Ramiro <moya04.ramiro@gmail.com>
2024-06-05 18:07:02 -0300

    1.3-1.5 0:30

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,74 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3;
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Part name={part1} exercisesTotal={exercises1}/>
-      <Part name={part2} exercisesTotal={exercises2}/>
-      <Part name={part3} exercisesTotal={exercises3}/>
-      <Total total={total} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
 
+
 const Header = (props) => {
 
+  console.log(props)
   return (
     <>
-      <p>Pertenece al curso: {props.course}</p>
+      <p>Pertenece al curso: {props.course.name}</p>
     </>
   )
 }
 
-const Part = (props) => {
+const Content = (props) => {
 
-  
+  console.log(props)
 
   return (
     <>
-      <p>EJERCICIO:</p>
-      <p>{props.name}</p>
+      {props.course.parts.map(part => (
+        <div key={part.name}> 
+        <p>EJERCICIO:</p>
+        <p>{part.name}</p>
+  
+        <p>Cantidad de ejercicios: {part.exercises}</p>
+        </div>
+      ))}
 
-      <p>Cantidad de ejercicios: {props.exercisesTotal}</p>
+      
     </>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
+
+  const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0);
+
+
   return (
     <>
-      <p>CANTIDAD TOTAL DE EJERCICIOS: {props.total}</p>
+      <p>CANTIDAD TOTAL DE EJERCICIOS: {total}</p>
     </>
   )
-  
+
 }
 
 export default App
